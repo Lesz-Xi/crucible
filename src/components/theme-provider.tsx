2292d7b6113f6028c1fcc940da9a15205c7339d9
Lesz <codewithafar@gmail.com>
2026-03-09 01:50:56 +0800

    feat(tokens): generate app css variables from figma tokens

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,26 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps, useTheme } from "next-themes";
+
+function ThemeAttributeSync() {
+  const { resolvedTheme } = useTheme();
+
+  React.useEffect(() => {
+    if (!resolvedTheme) return;
+
+    document.documentElement.setAttribute("data-theme", resolvedTheme);
+    document.body.setAttribute("data-theme", resolvedTheme);
+  }, [resolvedTheme]);
+
+  return null;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <ThemeAttributeSync />
+      {children}
+    </NextThemesProvider>
+  );
 }
